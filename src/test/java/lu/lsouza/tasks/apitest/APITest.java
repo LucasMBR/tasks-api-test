4cbaf950beff97202ca67ba8309c89a866d7ad1a
Lucas Souza <lucassouzaleal96@hotmail.com>
2023-02-02 15:48:11 -0300

    Alteração data atual

--- src/test/java/lu/lsouza/tasks/apitest/APITest.java
+++ src/test/java/lu/lsouza/tasks/apitest/APITest.java
@@ -27,7 +27,7 @@ public class APITest {
 	@Test
 	public void deveAdicionarTarefaComSucesso() {
 		RestAssured.given()
-			.body("{\"task\": \"Teste via API\", \"dueDate\": \"2023-02-01\"}")
+			.body("{\"task\": \"Teste via API\", \"dueDate\": \"2023-02-02\"}")
 			.contentType(ContentType.JSON)
 		.when()
 			.post("/todo")
